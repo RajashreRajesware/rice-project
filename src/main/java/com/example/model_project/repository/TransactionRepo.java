dc405aa0fd2d76cb3bf176324cce8bd83e22860d
Rajashre Rajesware <149167949+RajashreRajesware@users.noreply.github.com>
2025-12-09 12:18:07 +0530

    Update TransactionRepo.java

--- src/main/java/com/example/model_project/repository/TransactionRepo.java
+++ src/main/java/com/example/model_project/repository/TransactionRepo.java
@@ -1,7 +1,10 @@
 package com.example.model_project.repository;
 
 import com.example.model_project.entity.Transaction;
+import jakarta.persistence.Table;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -15,5 +18,19 @@ public interface TransactionRepo extends JpaRepository<Transaction,Long> {
 
     @Query("SELECT SUM(t.quantity) FROM Transaction t WHERE t.type = :type")
     Double getTotalQuantityByType(@Param("type") String type);
+
+
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Transaction t WHERE t.date BETWEEN :startDate AND :endDate")
+    int deleteByDateRange(@Param("startDate") LocalDate startDate,
+                          @Param("endDate") LocalDate endDate);
+
+
+    List<Transaction> findAllByDateBetween(LocalDate startDate, LocalDate endDate);
+
+    void deleteAllByIdIn(List<Long> ids);
+
 
 }
